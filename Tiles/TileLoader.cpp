91cbf9951c8f3465947d5435fd6461f80dd4a9a8
Dean M. Sands <deanmsands3@gmail.com>
2014-12-10 23:58:36 -0600

    Adding TileMap example from SFML wiki

---
+++ Tiles/TileLoader.cpp
@@ -0,0 +1,21 @@
+/*
+ * TileLoader.cpp
+ *
+ *  Created on: Dec 10, 2014
+ *      Author: Dean
+ */
+
+#include "TileLoader.h"
+
+namespace enDJIN {
+
+TileLoader::TileLoader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TileLoader::~TileLoader() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace enDJIN */
